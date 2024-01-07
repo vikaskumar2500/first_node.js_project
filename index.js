76d4997b57f1b642d0f89c5b20dc8960889e5346
vikaskumar2500 <vikas.nits8084@gamil.com>
2024-01-07 12:30:51 +0530

    complete req ans res

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-// console.log('Hello world!');
+const http = require("node:http");
 
-const fs = require('fs');
+const server = http.createServer((req, res) => {
+  if (req.url === "/home" || req.url === "/") {
+    res.write("<html>");
+    res.write("<head><title>HOME</title></head>");
+    res.write("<body><h2>Welcome Home</h2></body>");
+    res.write("</html>");
+    res.end();
+  } else if (req.url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>HOME</title></head>");
+    res.write("<body><h2>Welcome to About Us page</h2></body>");
+    res.write("</html>");
+    res.end();
+  } else if (req.url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>HOME</title></head>");
+    res.write("<body><h2> Welcome to my Node Js project</h2></body>");
+    res.write("</html>");
+    res.end();
+  }
+  res.end();
+});
 
-fs.writeFileSync('hellow.txt', 'Hello World'); 
+server.listen(3001);
