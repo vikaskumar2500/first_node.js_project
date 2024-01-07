6be9353aa2e66af0dc510ebdef3fdaf376f5fbfc
vikaskumar2500 <vikas.nits8084@gamil.com>
2024-01-07 18:55:50 +0530

    middleware tast

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res) => {
+  res.send(`
+    <form action='/add-product' method="POST">
+      <input id='product' name='product' type='text'/>
+      <input id='id' name='id' type='number'/>
+      <button type='submit'>Submit</button>
+    </form>
+  `);
+  console.log('body', req.body);
+});
+
+
+app.use((req, res) => {
   console.log("I am in the next middleware");
   res.send("<div>Hello world</div>");
 });
